35bbe23bb7e3985a941af74887d78ca7927e4ffa
олег <@DESKTOP-SSTKF2A>
2023-04-09 13:09:16 +0300

    Добавьте файлы проекта.

---
+++ laba 10/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_10
+{
+    public interface IWordGame
+    {
+        bool GuessWord(string word);
+        string GetHint();
+    }
+    public class WordGame : IWordGame
+    {
+        private readonly string _secretWord;
+        private readonly string[] _hints;
+
+        public WordGame(string secretWord, string[] hints)
+        {
+            _secretWord = secretWord;
+            _hints = hints;
+        }
+
+        public bool GuessWord(string word)
+        {
+            return string.Equals(word, _secretWord, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public string GetHint()
+        {
+            return _hints[new Random().Next(0, _hints.Length)];
+        }
+    }
+    public class WordGameProxy : IWordGame
+    {
+        private readonly IWordGame _wordGame;
+
+        public WordGameProxy(IWordGame wordGame)
+        {
+            _wordGame = wordGame;
+        }
+        public bool GuessWord(string word)
+        {
+            return _wordGame.GuessWord(word);
+        }
+        public string GetHint()
+        {
+            return _wordGame.GetHint();
+        }
+    }
+}
+
---
+++ laba 10/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba_10
+{
+    public partial class Form1 : Form
+    {
+        private readonly IWordGame _wordGame;
+        public Form1()
+        {
+            InitializeComponent();
+            var wordGame = new WordGame("чай", new[] { "Напій", "Чорний або зелений", "П'єш с бутербродами" });
+
+            _wordGame = new WordGameProxy(wordGame);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            var guess = textBox1.Text;
+
+            
+
+            if (_wordGame.GuessWord(guess))
+            {
+                MessageBox.Show("Правильно!");
+            }
+            else
+            {
+                label1.Text = $"Hint: {_wordGame.GetHint()}";
+            }
+        }
+    }
+}
